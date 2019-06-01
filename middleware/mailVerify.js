ed7cfdc4943940cacb021855f1b4c621dc50d6cd
Nilay Majorwar <nilaymajorwar@gmail.com>
2019-06-02 05:13:43 +0530

    Fixed token null check

--- middleware/mailVerify.js
+++ middleware/mailVerify.js
@@ -3,7 +3,7 @@ const config = require('config');
 
 function mailAuth(req, res, next) {
   const token = req.header('x-auth-token');
-  if (!token) return res.status(401).send('Access denied: Token not found');
+  if (token == 'null') return res.status(401).send('Access denied: Token not found');
 
   try {
     const decoded = jwt.verify(token, config.get('mailTokenKey'));
