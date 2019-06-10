21c5ee46361ab0e3e29f3073030a7af13a20f518
Chinmay Goyal <cg2chinmay@gmail.com>
2019-06-10 22:51:25 +0530

    Added project creation form

--- routes/projects.js
+++ routes/projects.js
@@ -4,8 +4,8 @@ const { Project} = require("../models/project");
 
 
 router.post('/createproject',async (req, res) => {
-    let title = req.body.title; 
-    let professor = req.body.professor; 
+    let title = req.body.title;
+    // let professor = req.body.professor;
     let no_openings = req.body.no_openings; 
     let description = req.body.description; 
     let eligibility = req.body.eligibility; 
@@ -14,7 +14,7 @@ router.post('/createproject',async (req, res) => {
 
     let project = new Project({
         title: title,
-        professor: professor,
+        // professor: professor,
         no_openings: no_openings,
         description: description,
         eligibility: eligibility,
@@ -25,10 +25,11 @@ router.post('/createproject',async (req, res) => {
 
     try{
         const result = await project.save();
+        res.send(result);
     }
     catch(err){
         res.send("Please fill the complete information");
-        console.log(err);
+        console.log(err.message);
     }
 
 });
