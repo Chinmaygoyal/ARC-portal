7626111ab1009704b3d05942896cd7fff2e07638
ayushkumar2000 <akak355@gmail.com>
2019-08-01 00:30:35 +0530

    Revert "https"
    This reverts commit baf759fcc9758a9988d12df960018f1bfe7ef3f0.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const https = require("https");
+const http = require("http");
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
@@ -53,7 +53,7 @@ mongoose.connect(
 
 // Start the server
 const PORT = process.env.PORT;
-const server = https.createServer(app);
+const server = http.createServer(app);
 server.listen(PORT, () => {
   console.log(`Server live at port ${PORT}.`);
 });
